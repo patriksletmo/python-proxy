f58f315aa97e1354c05f2566c78628ccfacea4f5
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-09-04 11:02:31 +0200

    Pass tests

--- Proxy.py
+++ Proxy.py
@@ -1,7 +1,6 @@
 import socket
 import threading
 
-
 HTML_CONTENT = b'\nThis is a plain text file with no bad words in it.\n\nYour Web browser should be able to display this page just fine.\n\n\n'
 
 SOME_CONTENT = b"""HTTP/1.1 200 OK
@@ -24,6 +23,7 @@ class Proxy:
     def __init__(self):
         self.host = '127.0.0.1'
         self.port = 1337
+        self.serversocket = None
 
     def get_proxy_url(self):
         return 'http://' + self.host + ':' + str(self.port)
@@ -36,21 +36,56 @@ class Proxy:
     def start(self):
         threading.Thread(target=self.runner).start()
 
+    def stop(self):
+        #self.serversocket.shutdown(socket.SHUT_RDWR)
+        self.serversocket.close()
+
     def runner(self):
         # Open an internet socket for TCP
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind((self.host, self.port))
-        serversocket.listen(5)  # Max 5 concurrent connections
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversocket.bind((self.host, self.port))
+        self.serversocket.listen(5)  # Max 5 concurrent connections
 
-        while True:
-            # accept connections from outside
-            (clientsocket, address) = serversocket.accept()
-            # now do something with the clientsocket
-            # in this case, we'll pretend this is a threaded server
-            #ct = client_thread(clientsocket)
-            #ct.run()
-            print('New connection from ' + str(address))
-            self.write_dummy_response(clientsocket)
+        try:
+            while True:
+                # accept connections from outside
+                (clientsocket, address) = self.serversocket.accept()
+                # now do something with the clientsocket
+                # in this case, we'll pretend this is a threaded server
+                # ct = client_thread(clientsocket)
+                # ct.run()
+                print('New connection from ' + str(address))
+                #self.write_dummy_response(clientsocket)
+                self.write_response(clientsocket)
+        except ConnectionAbortedError:
+            pass
+        except OSError:
+            pass
 
     def write_dummy_response(self, clientsocket):
         clientsocket.send(SOME_CONTENT)
+
+    def write_response(self, clientsocket):
+        request_content = clientsocket.recv(1024).decode()
+        hostname = self.extract_header(request_content, 'Host')
+
+        websocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        websocket.connect((hostname, 80))
+
+        websocket.send(bytes(request_content, 'UTF-8'))
+        response_content = websocket.recv(1024)
+        clientsocket.send(response_content)
+
+        websocket.shutdown(socket.SHUT_RDWR)
+        websocket.close()
+
+        clientsocket.close()
+
+    def extract_header(self, content, name):
+        full_name = '\r\n' + name + ':'
+        header_name_start = content.index(full_name)
+        header_value_start = header_name_start + len(full_name)
+        header_value_stop = content.index('\r\n', header_value_start)
+
+        return content[header_value_start:header_value_stop].strip()
--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -10,9 +10,18 @@ SIMPLE_TEXT_CONTENT = b'\nThis is a plain text file with no bad words in it.\n\n
 SIMPLE_HTML_CONTENT = b'<html>\n\n<title>\nGood HTML File Test for CPSC 441 Assignment 1\n</title>\n\n<body>\n<p>\nThis is a simple HTML file with no bad words in it.\n</p>\n\n<p>\nYour Web browser should be able to display this page just fine.\n</p>\n\n</body>\n\n</html>\n\n\n'
 
 proxy = Proxy()
-proxy.start()
+
+
 
 class TestProxy(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        proxy.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        proxy.stop()
+
     def test_simple_text_file(self):
         response = requests.get(SIMPLE_TEXT_FILE, proxies=proxy.get_proxies())
         self.assertEqual(SIMPLE_TEXT_CONTENT, response.content)
