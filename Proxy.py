c8128cb8079a865775b42ee30c424e8353f8dffb
Patrik Sletmo <patrik.sletmo@gmail.com>
2017-09-04 10:36:13 +0200

    Successfully respond to HTTP request

--- Proxy.py
+++ Proxy.py
@@ -1,28 +1,23 @@
 import socket
+import threading
 
-SOME_CONTENT = """
-HTTP/1.1 200 OK
+
+HTML_CONTENT = b'\nThis is a plain text file with no bad words in it.\n\nYour Web browser should be able to display this page just fine.\n\n\n'
+
+SOME_CONTENT = b"""HTTP/1.1 200 OK
 Date: Thu, 31 Aug 2017 07:14:03 GMT
 Server: Apache/2.4.6 (CentOS) OpenSSL/1.0.1e-fips PHP/5.4.16 mod_perl/2.0.10 Perl/v5.16.3
 Last-Modified: Thu, 31 Aug 2017 05:59:01 GMT
 ETag: "173-5580657531af7"
 Accept-Ranges: bytes
-Content-Length: 371
+Content-Length: """ + bytes(str(len(HTML_CONTENT)), 'UTF-8') + b"""
 Keep-Alive: timeout=5, max=100
 Connection: Keep-Alive
 Content-Type: text/html; charset=UTF-8
 
-
-<html>
-
-Congratulations again!  Now you've downloaded the file lab2-2.html. <br>
-This file's last modification date will not change.  <p>
-Thus  if you download this multiple times on your browser, a complete copy <br>
-will only be sent once by the server due to the inclusion of the IN-MODIFIED-SINCE<br>
-field in your browser's HTTP GET request to the server.
-
-</html>
 """
+
+SOME_CONTENT = SOME_CONTENT.replace(b'\n', b'\r\n') + HTML_CONTENT
 
 
 class Proxy:
@@ -39,6 +34,9 @@ class Proxy:
         }
 
     def start(self):
+        threading.Thread(target=self.runner).start()
+
+    def runner(self):
         # Open an internet socket for TCP
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serversocket.bind((self.host, self.port))
@@ -49,5 +47,10 @@ class Proxy:
             (clientsocket, address) = serversocket.accept()
             # now do something with the clientsocket
             # in this case, we'll pretend this is a threaded server
-            ct = client_thread(clientsocket)
-            ct.run()
+            #ct = client_thread(clientsocket)
+            #ct.run()
+            print('New connection from ' + str(address))
+            self.write_dummy_response(clientsocket)
+
+    def write_dummy_response(self, clientsocket):
+        clientsocket.send(SOME_CONTENT)
